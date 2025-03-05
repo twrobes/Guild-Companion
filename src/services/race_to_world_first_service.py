0983b699039123497fb9a83041199d6de60957e2
Thomas Wrobel <twrobel115@gmail.com>
2025-03-05 00:33:03 -0600

    - Updated get_formatted_number to encompass all numerical suffixes - Added heroic difficulty tracking - Increases the amount of ranks to track up to 50 guilds per boss

--- src/services/race_to_world_first_service.py
+++ src/services/race_to_world_first_service.py
@@ -7,6 +7,7 @@ import discord
 
 from env import POSTGRESQL_SECRET
 
+BOSS_KILL_AMOUNT_LIMIT = 50
 BOSS_SLUG_LIST = [
     'vexie-and-the-geargrinders',
     'cauldron-of-carnage',
@@ -29,56 +30,63 @@ BOSS_URL_LIST = [
     'https://i.imgur.com/jSp3TfY.png'
 ]
 CURRENT_RAID_SLUG = 'liberation-of-undermine'
-BOSS_KILL_AMOUNT_LIMIT = 20
+DIFFICULTY_LIST = ['mythic', 'heroic']
+
 
 async def retrieve_race_update(rwf_channel):
     update_dict = None
 
-    for boss_idx in range(len(BOSS_SLUG_LIST)):
-        get_boss_rank_url = f"https://raider.io/api/v1/raiding/boss-rankings?raid={CURRENT_RAID_SLUG}&boss={BOSS_SLUG_LIST[boss_idx]}&difficulty=mythic&region=world"
+    for difficulty in DIFFICULTY_LIST:
+        for boss_idx in range(len(BOSS_SLUG_LIST)):
+            get_boss_rank_url = f"https://raider.io/api/v1/raiding/boss-rankings?raid={CURRENT_RAID_SLUG}&boss={BOSS_SLUG_LIST[boss_idx]}&difficulty={difficulty}&region=world"
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(get_boss_rank_url) as response:
-                boss_rankings = await response.json()
+            async with aiohttp.ClientSession() as session:
+                async with session.get(get_boss_rank_url) as response:
+                    boss_rankings = await response.json()
 
-        if response.ok and boss_rankings is not None and len(boss_rankings) != 0:
-            update_dict: dict | None = await get_update_dict(BOSS_SLUG_LIST[boss_idx], boss_rankings['bossRankings'])
-        elif boss_rankings is None:
-            logging.error(f'JSON response was none. JSON content: {boss_rankings}')
-        elif 400 <= response.status < 500:
-            logging.error(f'Page or content was not found. Status code: {response.status}')
-        elif 500 <= response.status < 600:
-            logging.error(f'Raider.io is down, their server returned a status: {response.status}')
-        else:
-            logging.error('An unknown error occurred when requesting the raid statistic')
+            if response.ok and boss_rankings is not None and len(boss_rankings) != 0:
+                update_dict: dict | None = await get_update_dict(BOSS_SLUG_LIST[boss_idx], boss_rankings['bossRankings'], difficulty)
+            elif boss_rankings is None:
+                logging.error(f'JSON response was none. JSON content: {boss_rankings}')
+            elif 400 <= response.status < 500:
+                logging.error(f'Page or content was not found. Status code: {response.status}')
+            elif 500 <= response.status < 600:
+                logging.error(f'Raider.io is down, their server returned a status: {response.status}')
+            else:
+                logging.error('An unknown error occurred when requesting the raid statistic')
 
-        await asyncio.sleep(2)
+            await asyncio.sleep(2)
 
-        if update_dict is None:
-            continue
-        else:
-            update_msg = f'Guild <{update_dict["guild"]}> achieved the world {await get_formatted_number(update_dict["rank"])} kill of {update_dict["boss_name"]}!'
-            update_embed = discord.Embed(
-                color=discord.Color.dark_embed(),
-                title='Race to World First'
-            )
-            update_embed.add_field(name='**NEW KILL**', value=update_msg)
-            update_embed.set_image(url=BOSS_URL_LIST[boss_idx])
+            if update_dict is None:
+                continue
+            else:
+                update_msg = (f'**{update_dict["guild"]}** achieved the world {await get_formatted_number(update_dict["rank"])} kill of {difficulty.title()} '
+                              f'{update_dict["boss_name"]}!')
+                update_embed = discord.Embed(
+                    color=discord.Color.dark_embed(),
+                    title='Race to World First'
+                )
+                update_embed.add_field(name='**NEW KILL**', value=update_msg)
+                update_embed.set_image(url=BOSS_URL_LIST[boss_idx])
 
-            try:
-                if update_dict["guild_image_url"] is not None or len(update_dict["guild_image_url"]) != 0:
-                    update_embed.set_thumbnail(url=update_dict["guild_image_url"])
-            except Exception:
-                logging.warning(f"Something went wrong with the guild image url: {update_dict['guild_image_url']}")
+                try:
+                    if update_dict["guild_image_url"] is not None or len(update_dict["guild_image_url"]) != 0:
+                        update_embed.set_thumbnail(url=update_dict["guild_image_url"])
+                    else:
+                        update_embed.set_thumbnail(url='https://assets-prd.ignimgs.com/2023/11/03/warcraft1-1699040733114.jpg')
+                except Exception:
+                    logging.warning(f"Something went wrong with the guild image url: {update_dict['guild_image_url']}")
 
-            await rwf_channel.send(embed=update_embed)
+                await rwf_channel.send(embed=update_embed)
 
 
-async def get_update_dict(boss_slug: str, boss_rankings_json: dict):
+async def get_update_dict(boss_slug: str, boss_rankings_json: dict, difficulty):
+    boss = difficulty + '-' + boss_slug
+
     try:
         conn = await asyncpg.connect(f'postgres://avnadmin:{POSTGRESQL_SECRET}@atrocious-bot-db-atrocious-bot.l.aivencloud.com:12047/defaultdb?sslmode=require')
         get_record_query = """SELECT kills FROM rwf_tracker WHERE boss=($1)"""
-        result = await conn.fetch(get_record_query, boss_slug)
+        result = await conn.fetch(get_record_query, boss)
         boss_kills = result[0]['kills']
     except (Exception, asyncpg.PostgresError) as e:
         logging.error(f'The database transaction to retrieve {boss_slug} boss record had an error: {e}')
@@ -95,7 +103,7 @@ async def get_update_dict(boss_slug: str, boss_rankings_json: dict):
 
     try:
         update_record_query = f"""UPDATE rwf_tracker SET kills=$1 WHERE boss=$2"""
-        await conn.execute(update_record_query, boss_kills, boss_slug)
+        await conn.execute(update_record_query, boss_kills, boss)
     except (Exception, asyncpg.PostgresError) as e:
         logging.error(f'The database transaction to update boss kills had an error: {e}')
         return None
@@ -108,26 +116,40 @@ async def get_update_dict(boss_slug: str, boss_rankings_json: dict):
         logging.info('Tried to get an invalid index from boss_rankings_json')
         return None
 
+    try:
+        guild_image_url = target_rank['guild']['logo']
+    except KeyError:
+        guild_image_url = None
+
     return {
         "boss_name": boss_slug.replace("-", " ").title(),
         "guild": target_rank['guild']['name'],
-        "guild_image_url": target_rank['guild']['logo'],
+        "guild_image_url": guild_image_url,
         "rank": target_rank['rank']
     }
 
 
-async def get_formatted_number(rank: int):
-    match rank:
-        case 1:
-            return '1st'
-        case 2:
-            return '2nd'
-        case 3:
-            return '3rd'
-        case 4:
-            return '4th'
-        case 5:
-            return '5th'
+async def get_formatted_number(rank: int) -> str:
+    rank_str = str(rank)
+
+    if rank_str == '11':
+        return rank_str + 'th'
+
+    if rank_str == '12':
+        return rank_str + 'th'
+
+    if rank_str == '13':
+        return rank_str + 'th'
+
+    match rank_str[-1]:
+        case '1':
+            return rank_str + 'st'
+        case '2':
+            return rank_str + 'nd'
+        case '3':
+            return rank_str + 'rd'
+        case '0', '4', '5', '6', '7', '8', '9':
+            return rank_str + 'th'
         case _:
             logging.error(f'Got an invalid value for boss rank: {rank}')
             return 'rank'
